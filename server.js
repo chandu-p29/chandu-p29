c1f4c872ad4c21b7ef9ca1ec0824442edbdef4b7
Chandhu Parameswaran <chanduparameswaran@gmail.com>
2022-09-28 00:38:59 +0200

    updated server.js response

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('API Running'));
+app.get('/', (req, res) => res.send('API Running successfully'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server started on port ', PORT) );
