6ee4084ea87afe2250263937a89f130ad9ae0712
Chandhu Parameswaran <chanduparameswaran@gmail.com>
2022-09-28 14:59:26 +0200

    Set database connection

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+//connect DB
+const connectDB = require('./config/db');
+connectDB();
 app.get('/', (req, res) => res.send('API Running successfully'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server started on port ', PORT) );
