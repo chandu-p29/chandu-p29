c893dc82e8ded7f96c3c37149f6f61dec65bee44
Chandhu Parameswaran <chanduparameswaran@gmail.com>
2022-10-08 14:39:05 +0200

    implimented JWT token

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const { check, validationResult } = require('express-validator/check');
 const User = require('../../models/User')
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 // @route    Post api/users
 // @desc     User Registration
 // @access   Public
@@ -39,7 +41,19 @@ async (req, res) => {
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt);
         await user.save();
-        res.send('User Registered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            {  expiresIn: 36000 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            });
     }catch(err) {
         console.error(err.message);
         res.status(500).send('Server Error');
